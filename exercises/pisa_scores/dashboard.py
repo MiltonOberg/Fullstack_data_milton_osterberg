90df333d46b27cc5b4d6126b1ef880279300a45f
Milton Österberg <miltonwendel@outlook.com>
2024-09-11 14:01:46 +0200

    exercise 2

--- exercises/pisa_scores/dashboard.py
+++ exercises/pisa_scores/dashboard.py
@@ -17,8 +17,27 @@ def run_dashboard():
     
     st.markdown("# PISA Scores")
     st.dataframe(df)
-    basic_stats= pd.DataFrame(data= df, columns=)
+    basic_stats= pd.DataFrame({"Records": [df["index"].count()], 
+                               "Locations": [df["LOCATION"].count()], 
+                               "Subjects": [df["SUBJECT"].count()],
+                               "Avg time": [round(df["TIME"].mean())]
+                               })
     st.dataframe(basic_stats)
+    
+    score_location= df.groupby("LOCATION")["Value"].mean().reset_index()
+    fig= px.bar(score_location, x= "LOCATION", y= "Value", title= "Avg score per country")
+    st.plotly_chart(fig)
+    
+    country= st.selectbox("Choose a Counrty", score_location["LOCATION"])
+    
+    country_df= df[df["LOCATION"]== country]
+    
+    
+    time_value= country_df.groupby("TIME", as_index= False)["Value"].mean()
+        
+    
+    fig= px.line(time_value, x= "TIME", y= "Value")
+    st.plotly_chart(fig)
     
 if __name__ == "__main__":
     run_dashboard()
