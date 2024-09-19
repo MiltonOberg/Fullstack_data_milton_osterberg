ebbba5ce15d0836a89c28dda03a22d196a5c66a3
Milton Österberg <miltonwendel@outlook.com>
2024-09-19 13:52:12 +0200

    exercise 3.0

--- exercises/exercise_3/Scripts/EDA.sql
+++ exercises/exercise_3/Scripts/EDA.sql
@@ -1,1 +1,51 @@
 SELECT * FROM games;
+
+
+SELECT
+	DISTINCT winner
+FROM
+	games;
+
+
+SELECT
+	DISTINCT victory_status
+FROM
+	games;
+
+
+SELECT
+	COUNT(id)
+FROM
+	games;
+
+
+SELECT
+	COUNT(winner)
+FROM
+	games
+GROUP BY
+	winner;
+
+
+SELECT
+	opening_name,
+	COUNT(*) AS count
+FROM
+	games
+GROUP BY
+	opening_name
+ORDER BY
+	count DESC
+LIMIT 10;
+
+
+SELECT
+	GREATEST(white_rating,
+	black_rating) AS highest_rating
+FROM
+	games
+ORDER BY
+	highest_rating DESC
+LIMIT 1;
+
+
