f509527b3733a72174d012be147972cd70e610f3
Milton Österberg <miltonwendel@outlook.com>
2024-10-09 09:26:31 +0200

    hejsan du

--- 10_lab_overview/backend/constants.py
+++ 10_lab_overview/backend/constants.py
@@ -1,6 +1,7 @@
 from pathlib import Path
 
-DATABASE_PATH = Path(__file__).parent  /"youtube_data.db"
-CLEANED_DATA_PATH = Path(__file__).parent  / "cleaned_data"
-RAW_DATA_PATH = Path(__file__).parent / "raw_data"
+DATABASE_PATH = Path(__file__).parent/ "youtube_data.db"
+CLEANED_DATA_PATH = Path(__file__).parent / "cleaned_data"
+RAW_DATA_PATH = Path(__file__).parent/ "raw_data"
 CSS_PATH= Path(__file__).parents[1]/ "frontend"/ "style.css"
+BACKGOUND_IMAGE= Path(__file__).parents[1]/ "frontend"/ "dashboard_background.png"
