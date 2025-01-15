8743a5458b612a96095b4a93848c6f5c7d838dab
Milton Österberg <miltonwendel@outlook.com>
2025-01-15 10:49:56 +0100

    code along 12

--- code_alongs/12_packaging_python_project/setup.py
+++ code_alongs/12_packaging_python_project/setup.py
@@ -3,3 +3,23 @@ from setuptools import find_packages
 
 
 print(find_packages(exclude=["test*", "explorations"]))
+
+setup(
+    name="travel_planner",
+    version="0.0.1",
+    description="""
+    This package is used for travel planning in public trasport
+    """,
+    author="Milton Österberg",
+    author_email="mille96402_boy153135.gmail.com",
+    install_packages=[
+        "streamlit",
+        "pandas",
+        "requests",
+        "folium",
+        "pprint",
+        "python-dotenv",
+    ],
+    packages=find_packages(exclude=["test*", "explorations"]),
+    entry_points= {"console_scripts": ["dashboard = utils.run_dashboard:run_dashboard"]}
+)
